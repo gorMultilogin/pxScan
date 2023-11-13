d2dd35fb89dd41cfe3997177382393eed9462050
Gor Petrosyan <gorpetrosyan@minis-MacBook-Pro.local>
2023-11-13 16:01:04 +0400

    init

---
+++ src/js/hexadecimal.js
@@ -0,0 +1,15 @@
+import {listFunc} from "@/js/list";
+export function hexadecimal(_0x55ba52, _0x78b282) {
+    const _0x292130 = listFunc();
+     const hexadecimalData = (
+        (hexadecimal = function (_0x45d891, _0x383ad1) {
+            _0x45d891 = _0x45d891 - 0x11a;
+            let _0x51baee = _0x292130[_0x45d891];
+            return _0x51baee;
+        }),
+            hexadecimal(_0x55ba52, _0x78b282)
+    );
+    // console.log({arg1: _0x55ba52, arg2: _0x78b282})
+    // console.log({hexadecimalData})
+     return hexadecimalData;
+}
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+
+const listOfNoneProperties = {};
+for (var props in window) {
+    if(!window.hasOwnProperty(props)){
+        if(typeof window[props] === 'function'){
+            listOfNoneProperties[props] = window[props];
+
+        }else{
+            listOfNoneProperties[props] = window[props];
+        }
+    }
+}
+console.log({listOfNoneProperties})
+console.log({self})
